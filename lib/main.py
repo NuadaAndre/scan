970570b3cb85f1585d47780572f2f53010f359a5
xiaofan <xiaofan_bp@163.com>
2017-06-02 15:45:52 +0800

    优化 main

--- lib/main.py
+++ lib/main.py
@@ -1,43 +1,38 @@
 # coding=utf-8
 from Queue import Queue
+import sys
 import options
 import getTask
 import scan
-import sys
-import os
-
-# 获取参数
-args = options.oparser()
-
-# 获取任务
-try:
-    qtask = getTask.getTask(args.urls,args.pocfile)
-    # qsize = qtask.qsize()
-    # while not qtask.empty():
-    #     print qtask.get() 
-except TypeError as e:
-    print e
-    sys.exit(0)
-except Exception as e:
-    print e
-    sys.exit(0)
-
-# 启动扫描
-qresults = Queue()
-scan.main(qtask,qresults,args.thread)
+import tools
 
 
-# 保存结果
-if args.out:
-    if '/' in args.out  or '\\' in args.out:
-        path = os.path.dirname(args.out)
-        if not os.path.exists(path):
-            os.makedirs(path)
-    with open(args.out,'w') as f:
-        while not qresults.empty():
-            result = qresults.get()
-            f.write(result['info'])
-            f.write('\n')
+def scanpoc():
 
+    try:
+        # 获取参数
+        args = options.oparser()
+
+        # 获取任务
+        qtask = getTask.getTask(args.urls,args.pocfile)
+
+        # 启动扫描
+        qresults = Queue()
+        scan.main(qtask,qresults,args.thread)
+
+        # 保存结果
+        if args.out:
+            tools.set2txt(args.out,qresults)
+
+    except TypeError as e:
+        print e
+        sys.exit(0)
+    except Exception as e:
+        print e
+        sys.exit(0)
+
+    
+    
+scanpoc()
 
 
